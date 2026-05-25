5bdb4f5cb7d51db1cc0ec4ec66f3dd02ab681f94
Ebube Williams <chidiekwebelam@gmail.com>
2026-05-25 09:51:49 +0100

    playground

---
+++ javascript/playground.js
@@ -0,0 +1,33 @@
+/**
+ *
+ *  Student Assignment for Monday 24th May 2026.
+ *
+ *  Do a deep study for presentation on command line arguments and process.argv in JavaScript. Then, write a code snippet that demonstrates the use of command line arguments in JavaScript. You can use the process.argv array to access the command line arguments passed to your script. Make sure to include comments in your code to explain how it works.
+ *
+ */
+
+/*The process.argv in javascript is used to access and interact with the command-line arguments. It is an array that contains the command line arguments and helps user to interact node app using CLI. */
+
+/*This property returns an array containing the arguments passed to the process when run in the command line.
+
+process.argv[0]: is the process execution path
+process.argv[1]: is the path for the js file.
+process.argv[2] and more: Are items in the array arguments passed by the user. */
+
+/*What Are Command Line Arguments?
+Command line arguments are values passed to a program when it is executed from the terminal. */
+
+/*Below examples illustrate the use of process.argv property in Node.js:
+
+Example 1: The below example uses process.argv property to display the CLI argument passed by the user */
+
+const name = process.argv[2];
+// console.log(`Hello ${name}, How was your weekend?`);
+
+/*Example 2: Converting Arguments to Numbers 
+Note: Arguments are always recieved as strings*/
+
+const num1 = Number(process.argv[2]);
+const numb2 = Number(process.argv[3]);
+
+console.log(num1 + numb2);
