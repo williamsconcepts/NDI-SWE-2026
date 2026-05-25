8cb7440a3a23594b248890ac6d62fa8027e20b22
williamsconcepts <chidiekwebelam@gmail.com>
2026-05-25 09:48:49 +0100

    stash

--- javascript/01-intro/01-intro.js
+++ javascript/01-intro/01-intro.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-console.log("This is an external javascript file");
-
-console.log("I am learning Javascript, all thanks to Mr.Hycient")
-=======
 console.log("This is an external JavaScript file.");
->>>>>>> 089292008401f3736851002873f61f5c0ece8ffb
